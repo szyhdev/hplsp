1a4ffa64282e118ffdfef6f2b60c06187dac9ee3
rhd <szyuanhua@163.com>
2026-06-29 21:15:35 +0800

    03_testlisten: Show how backlog works

---
+++ 05/03_testlisten.cpp
@@ -0,0 +1,64 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+static bool stop = false;
+
+static void handle_term(int sig)
+{
+    printf("SIGTERM received\n");
+    stop = true;
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGTERM, handle_term);
+
+    if (argc <= 3) {
+        printf("usage: %s ip_address port_number backlog\n", basename(argv[0]));
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int backlog = atoi(argv[3]);
+
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("create socket error: %d\n", errno);
+        return -1;
+    }
+
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+    addr.sin_port = htons(port);
+
+    int ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret == -1) {
+        printf("bind socket error: %d\n", errno);
+        close(sockfd);
+        return -1;
+    }
+
+    ret = listen(sockfd, backlog);
+    if (ret == -1) {
+        printf("listen socket error: %d\n", errno);
+        close(sockfd);
+        return -1;
+    }
+
+    while (!stop) {
+        sleep(1);
+    }
+
+    close(sockfd);
+
+    return 0;
+}
+
+// gcc -o 03_testlisten 03_testlisten.cpp
