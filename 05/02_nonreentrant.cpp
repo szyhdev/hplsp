b51332dbc9c3190dbf7eef953510ed6b0b304e07
rhd <szyuanhua@163.com>
2026-06-29 21:12:46 +0800

    02_nonreentrant: Show inet_ntoa is non-reentrant

---
+++ 05/02_nonreentrant.cpp
@@ -0,0 +1,24 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+
+void non_reentrant()
+{
+    struct in_addr addr1, addr2;
+    addr1.s_addr = inet_addr("192.168.1.1");
+    addr2.s_addr = inet_addr("10.0.0.1");
+
+    char *str1 = inet_ntoa(addr1);
+    char *str2 = inet_ntoa(addr2);
+
+    printf("addr1: %s\n", str1);
+    printf("addr2: %s\n", str2);
+}
+
+int main()
+{
+    non_reentrant();
+
+    return 0;
+}
+
+// gcc -o 02_nonreentrant 02_nonreentrant.cpp
