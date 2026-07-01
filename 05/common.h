b60415fd51fe0a48fd20406e049fe93c3968c208
rhd <szyuanhua@163.com>
2026-07-01 17:33:25 +0800

    Move common header files to common.h

---
+++ 05/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
