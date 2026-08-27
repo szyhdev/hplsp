0f9c2ec53350b8a818777851dd8a878e12dda056
rhd <szyuanhua@163.com>
2026-08-27 10:34:17 +0800

    01_sockaddr: Update include files

--- 05_fundamentals_of_network_programming/common.h
+++ 05_fundamentals_of_network_programming/common.h
@@ -4,9 +4,11 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <netdb.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
