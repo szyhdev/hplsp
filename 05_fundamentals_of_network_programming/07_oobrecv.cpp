45e2ca4ee2a0e39728b676d01cf9f5475b44f6d8
rhd <szyuanhua@163.com>
2026-08-28 17:37:30 +0800

    07_oobrecv: Use SIGURG for OOB data

--- 05_fundamentals_of_network_programming/07_oobrecv.cpp
+++ 05_fundamentals_of_network_programming/07_oobrecv.cpp
@@ -2,7 +2,7 @@
 
 // #define USE_SIGURG
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 int connfd;
 
@@ -10,15 +10,20 @@ int connfd;
 
 void sigurg_handler(int sig)
 {
-    printf("SIGURG received\n");
-    if (sockatmark(connfd) == 1) {
-        char oob_buf[64];
-        memset(oob_buf, '\0', sizeof(oob_buf));
-        int ret = recv(connfd, &oob_buf, sizeof(oob_buf) - 1, MSG_OOB);
-        if (ret > 0) {
-            oob_buf[ret] = '\0';
-            printf("got %d bytes of oob data '%s'\n", ret, oob_buf);
-        }
+    char oob_data;
+    int ret = recv(connfd, &oob_data, sizeof(oob_data), MSG_OOB);
+    if (ret == -1) {
+        const char *msg = "recv socket error\n";
+        write(STDOUT_FILENO, msg, strlen(msg));
+    } else if (ret == 0) {
+        const char *msg = "connection closed by client\n";
+        write(STDOUT_FILENO, msg, strlen(msg));
+    } else {
+        const char *msg1 = "got 1 bytes of oob data: '";
+        const char *msg2 = "'\n";
+        write(STDOUT_FILENO, msg1, strlen(msg1));
+        write(STDOUT_FILENO, &oob_data, sizeof(oob_data));
+        write(STDOUT_FILENO, msg2, strlen(msg2));
     }
 }
 
@@ -33,17 +38,26 @@ int main(int argc, char *argv[])
     const char *ip = argv[1];
     int port = atoi(argv[2]);
 
-    struct sockaddr_in addr;
-    bzero(&addr, sizeof(addr));
-    addr.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &addr.sin_addr);
-    addr.sin_port = htons(port);
+#if defined(USE_SIGURG)
+    signal(SIGURG, sigurg_handler);
+#endif
 
     int sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         printf("create socket error: %d\n", errno);
         return -1;
     }
+
+    if (setreuseaddr(sockfd) == -1) {
+        close(sockfd);
+        return -1;
+    }
+
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+    addr.sin_port = htons(port);
 
     int ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     if (ret == -1) {
@@ -60,8 +74,8 @@ int main(int argc, char *argv[])
     }
 
     struct sockaddr_in client_addr;
-    socklen_t client_addrlength = sizeof(client_addr);
-    connfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addrlength);
+    socklen_t client_addrlen = sizeof(client_addr);
+    connfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addrlen);
     if (connfd == -1) {
         printf("accept socket error: %d\n", errno);
         close(sockfd);
@@ -69,29 +83,28 @@ int main(int argc, char *argv[])
     }
 
 #if defined(USE_SIGURG)
-
     fcntl(connfd, F_SETOWN, getpid());
-    signal(SIGURG, sigurg_handler);
-
 #endif
 
-    char buffer[BUF_SIZE];
+    char buffer[BUFFER_SIZE];
 
-    memset(buffer, '\0', BUF_SIZE);
-    ret = recv(connfd, buffer, BUF_SIZE - 1, 0);
-    printf("got %d bytes of normal data '%s'\n", ret, buffer);
+    memset(buffer, '\0', BUFFER_SIZE);
+    ret = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
+    printf("got %d bytes of normal data: '%s'\n", ret, buffer);
 
 #if !defined(USE_SIGURG)
-
-    memset(buffer, '\0', BUF_SIZE);
-    ret = recv(connfd, buffer, BUF_SIZE - 1, MSG_OOB);
-    printf("got %d bytes of oob data '%s'\n", ret, buffer);
-
+    memset(buffer, '\0', BUFFER_SIZE);
+    ret = recv(connfd, buffer, BUFFER_SIZE - 1, MSG_OOB);
+    printf("got %d bytes of oob data: '%s'\n", ret, buffer);
 #endif
 
-    memset(buffer, '\0', BUF_SIZE);
-    ret = recv(connfd, buffer, BUF_SIZE - 1, 0);
-    printf("got %d bytes of normal data '%s'\n", ret, buffer);
+    memset(buffer, '\0', BUFFER_SIZE);
+    ret = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
+    printf("got %d bytes of normal data: '%s'\n", ret, buffer);
+
+    memset(buffer, '\0', BUFFER_SIZE);
+    ret = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
+    printf("got %d bytes of normal data: '%s'\n", ret, buffer);
 
     close(connfd);
     close(sockfd);
